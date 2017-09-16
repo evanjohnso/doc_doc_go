238844b69f5523688fdfabde9aecf0f386b8073c
ej <evanrjohnso@gmail.com>
2017-09-15 20:40:53 -0700

    api request to populate dropdown

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,18 +1,54 @@
-import { DocDoc } from './../js/logic.js';
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-alert('doc loaded');
+promiseOne()
+  .then(displayDropdown)              //displayDropdown callback function
+  .catch(function(errorObject) {      //catch any rejections of promises
+    alert(errorObject);
+  });
   $("#userInput").submit(function(evt) {
     evt.preventDefault();
     evt.stopImmediatePropagation();
     evt.stopPropagation();
-    alert('form submit');
     let location = $('input[name="userInputLocation"]').val();
     let condition = $('input[name="userInputCondition"]').val();
-    console.log(location);
-    console.log(condition);
     $('input').val('');
-    DocDoc.homies();
+
+
   });
 
+
+
+
+
+  function displayDropdown(jsonArray) {
+      //split by dashes, join by spaces,
+      //uppercase first letter
+      let sorted = jsonArray.data.map( function(element) {
+        let changing = element.uid.split('-').join(' ');
+        return changing.toString().charAt(0).toUpperCase() + changing.toString().slice(1);
+      });
+      //alpha sort and append list options
+      sorted.sort();
+      sorted.forEach(function(element) {
+        $('select[name="specialty"]').append(`<option value="${element}">${element}</option>`);
+    });
+  }
+  function promiseOne() {
+      return new Promise( function(resolve, reject) {
+      $.ajax( {
+        url: `https://api.betterdoctor.com/2016-03-01/specialties?limit=100&user_key=${apiKey}`,
+        type: "GET",
+        data: {
+          format: 'json'
+        },
+        success: function(responseObject) {
+          resolve(responseObject);
+        },
+        error: function(error) {
+          reject("API request for physician specialties failed, our bad");
+        }
+      });
+    });
+  }
 });
