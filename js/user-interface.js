ad867cff098beaf34b83c9868d1009059bfce45a
ej <evanrjohnso@gmail.com>
2017-09-15 21:44:03 -0700

    working on latlon promise chain with specific doctors

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,7 +1,7 @@
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-promiseOne()
+getSpeciality()
   .then(displayDropdown)              //displayDropdown callback function
   .catch(function(errorObject) {      //catch any rejections of promises
     alert(errorObject);
@@ -11,11 +11,49 @@ promiseOne()
     evt.stopImmediatePropagation();
     evt.stopPropagation();
     let location = $('input[name="userInputLocation"]').val();
-    let condition = $('input[name="userInputCondition"]').val();
+    // let condition = $('input[name="userInputCondition"]').val();
+    let specialty = $('select[name="specialty"]').val();
+    getLatLon(location, specialty);
+    console.log(specialty);
     $('input').val('');
-
-
   });
+
+  function getLatLon(location, specialty) {
+    return new Promise( function(resolve, reject) {
+    let geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { address: location }, function(results) {
+      let lat = results[0].geometry.location.lat();
+      let lng = results[0].geometry.location.lng();
+      resolve(lat + ',' + lng, specialty);
+      reject("Google's geocoder failed, the servers are down. We're all fucked.");
+      });
+    });
+  }
+
+  getLatLon("portland, or")
+    .then(getDoctors)
+    .catch( function(error) {
+      alert(error);
+    });
+
+  function getDoctors(searchLocation, speciality) {
+    return new Promise( function(resolve, reject) {
+      $.ajax( {
+        url: `https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${speciality}&location=${searchLocation}&sort=distance-asc&skip=0&limit=20&user_key=${apiKey}`,
+        type: "GET",
+        data: {
+          format: 'json'
+        },
+        success: function(responseObject) {
+          console.log('finished promise chain');
+          resolve(responseObject);
+        },
+        error: function(error) {
+          reject("API request for physician specialties failed, our bad");
+        }
+      });
+    });
+  }
 
 
 
@@ -34,7 +72,7 @@ promiseOne()
         $('select[name="specialty"]').append(`<option value="${element}">${element}</option>`);
     });
   }
-  function promiseOne() {
+  function getSpeciality() {
       return new Promise( function(resolve, reject) {
       $.ajax( {
         url: `https://api.betterdoctor.com/2016-03-01/specialties?limit=100&user_key=${apiKey}`,
