f896fba294acb4be2b07f70b7e2f23c89d32d518
ej <evanrjohnso@gmail.com>
2017-09-16 21:03:07 -0700

    display doctors to front end

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,11 +1,13 @@
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
+
 getSpeciality()
-  .then(displayDropdown)              //displayDropdown callback function
-  .catch(function(errorObject) {      //catch any rejections of promises
-    alert(errorObject);
-  });
+  .then(displayDropdown);              //displayDropdown callback function
+  // .catch(function(errorObject) {      //catch any rejections of promises
+  //   alert(errorObject);
+  // });
+
   $("#userInput").submit(function(evt) {
     evt.preventDefault();
     evt.stopImmediatePropagation();
@@ -13,40 +15,47 @@ getSpeciality()
     let location = $('input[name="userInputLocation"]').val();
     // let condition = $('input[name="userInputCondition"]').val();
     let specialty = $('select[name="specialty"]').val();
-    getLatLon(location, specialty);
-    console.log(specialty);
+    // console.log(specialty);
+    let latLon = '45.5230622,-122.67648150000002,15';
+    // getLatLon(location, specialty);
+    // getDoctors(latLon, specialty);
     $('input').val('');
   });
 
-  function getLatLon(location, specialty) {
-    return new Promise( function(resolve, reject) {
-    let geocoder = new google.maps.Geocoder();
-    geocoder.geocode( { address: location }, function(results) {
-      let lat = results[0].geometry.location.lat();
-      let lng = results[0].geometry.location.lng();
-      resolve(lat + ',' + lng, specialty);
-      reject("Google's geocoder failed, the servers are down. We're all fucked.");
-      });
-    });
-  }
+  // function getLatLon(location, specialty) {
+  //   return new Promise( function(resolve, reject) {
+  //   let geocoder = new google.maps.Geocoder();
+  //   geocoder.geocode( { address: location }, function(results) {
+  //     let lat = results[0].geometry.location.lat();
+  //     let lng = results[0].geometry.location.lng();
+  //     let coordinates = `${lat},${lng},15`; //last parameter is the search radius
+  //     console.log(coordinates);
+  //     resolve(coordinates, specialty);
+  //     reject("Google's geocoder failed, the servers are down. We're all fucked.");
+  //     });
+  //   });
+  // }
+  // getLatLon("Portland, OR");
 
-  getLatLon("portland, or")
-    .then(getDoctors)
-    .catch( function(error) {
-      alert(error);
-    });
+
+  // getLatLon("portland, or", "dentist")
+  //   .then(getDoctors)
+  //   .catch( function(error) {
+  //     console.log('this is the error' + error);
+  //   });
+  // getDoctors(latLon, 'acupuncture')
+  //   .then(displayDoctors);
 
   function getDoctors(searchLocation, speciality) {
     return new Promise( function(resolve, reject) {
       $.ajax( {
-        url: `https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${speciality}&location=${searchLocation}&sort=distance-asc&skip=0&limit=20&user_key=${apiKey}`,
+        url: `https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${speciality}&location=${searchLocation}&sort=distance-asc&skip=0&limit=10&user_key=${apiKey}`,
         type: "GET",
         data: {
           format: 'json'
         },
         success: function(responseObject) {
-          console.log('finished promise chain');
-          resolve(responseObject);
+          resolve(responseObject.data);
         },
         error: function(error) {
           reject("API request for physician specialties failed, our bad");
@@ -54,11 +63,19 @@ getSpeciality()
       });
     });
   }
+  function displayDoctors(jsonArray) {
+    console.log(jsonArray);
+    jsonArray.forEach(function(doctor) {
+        $('#outputs').append( `<div class='item'>
+                                  <h3>${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</h3>
+                                  <h4>${doctor.specialties[0].name}</h4>
+                                  <img src="${doctor.profile.image_url}" alt='No picture to display'>
+                                  <h5>${doctor.profile.bio}</h5>
+                              </div>`);
 
-
-
-
-
+      console.log(doctor);
+    });
+  }
   function displayDropdown(jsonArray) {
       //split by dashes, join by spaces,
       //uppercase first letter
@@ -75,7 +92,7 @@ getSpeciality()
   function getSpeciality() {
       return new Promise( function(resolve, reject) {
       $.ajax( {
-        url: `https://api.betterdoctor.com/2016-03-01/specialties?limit=100&user_key=${apiKey}`,
+        url: `https://api.betterdoctor.com/2016-03-01/specialties?limit=50&user_key=${apiKey}`,
         type: "GET",
         data: {
           format: 'json'
@@ -89,4 +106,6 @@ getSpeciality()
       });
     });
   }
+
+
 });
