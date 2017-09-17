db50f454fd69bb06afefe2f89bdea3e1bd60efd0
ej <evanrjohnso@gmail.com>
2017-09-17 15:29:02 -0700

    reformatted ajax requests

--- js/user-interface.js
+++ js/user-interface.js
@@ -3,39 +3,44 @@ var apiKey = require('./../.env').apiKey;
 $(document).ready(function() {
 
 getSpeciality()
-  .then(displayDropdown);              //displayDropdown callback function
-  // .catch(function(errorObject) {      //catch any rejections of promises
-  //   alert(errorObject);
-  // });
+  .then(displayDropdown)              //displayDropdown callback function
+  .catch(function(errorObject) {      //catch any rejections of promises
+    alert(errorObject);
+  });
 
   $("#userInput").submit(function(evt) {
     evt.preventDefault();
     evt.stopImmediatePropagation();
     evt.stopPropagation();
-    let location = $('input[name="userInputLocation"]').val();
+    // let location = $('input[name="userInputLocation"]').val();
     // let condition = $('input[name="userInputCondition"]').val();
     let specialty = $('select[name="specialty"]').val();
     // console.log(specialty);
     let latLon = '45.5230622,-122.67648150000002,15';
-    // getLatLon(location, specialty);
+    let location = "portland, or";
+    getLatLon(location, specialty)
+      .then(getDoctors)
+      .then(displayDoctors)
+      .catch( function (error) {
+        alert("Sorry, there seems to be an error", error);
+      })
     // getDoctors(latLon, specialty);
     $('input').val('');
   });
 
-  // function getLatLon(location, specialty) {
-  //   return new Promise( function(resolve, reject) {
-  //   let geocoder = new google.maps.Geocoder();
-  //   geocoder.geocode( { address: location }, function(results) {
-  //     let lat = results[0].geometry.location.lat();
-  //     let lng = results[0].geometry.location.lng();
-  //     let coordinates = `${lat},${lng},15`; //last parameter is the search radius
-  //     console.log(coordinates);
-  //     resolve(coordinates, specialty);
-  //     reject("Google's geocoder failed, the servers are down. We're all fucked.");
-  //     });
-  //   });
-  // }
-  // getLatLon("Portland, OR");
+  function getLatLon(location, specialty) {
+    return new Promise( function(resolve, reject) {
+    let geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { address: location }, function(results) {
+      let lat = results[0].geometry.location.lat();
+      let lng = results[0].geometry.location.lng();
+      let coordinates = `${lat},${lng},15`; //last parameter is the search radius
+      console.log(coordinates);
+      resolve(coordinates, specialty);
+      reject("Google's geocoder failed, the servers are down. We're all fucked.");
+      });
+    });
+  }
 
 
   // getLatLon("portland, or", "dentist")
@@ -49,11 +54,17 @@ getSpeciality()
   function getDoctors(searchLocation, speciality) {
     return new Promise( function(resolve, reject) {
       $.ajax( {
-        url: `https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${speciality}&location=${searchLocation}&sort=distance-asc&skip=0&limit=10&user_key=${apiKey}`,
-        type: "GET",
+        url: `https://api.betterdoctor.com/2016-03-01/doctors`,
+        method: "GET",
         data: {
-          format: 'json'
+          speciality_uid: speciality,
+          location: searchLocation,
+          sort: 'distance-asc',
+          skip: 0,
+          limit: '15',
+          user_key: apiKey
         },
+        dataType: 'json',
         success: function(responseObject) {
           resolve(responseObject.data);
         },
@@ -92,11 +103,13 @@ getSpeciality()
   function getSpeciality() {
       return new Promise( function(resolve, reject) {
       $.ajax( {
-        url: `https://api.betterdoctor.com/2016-03-01/specialties?limit=50&user_key=${apiKey}`,
-        type: "GET",
+        url: `https://api.betterdoctor.com/2016-03-01/specialties`,
+        method: "GET",
         data: {
-          format: 'json'
+          limit: 50,
+          user_key: apiKey,
         },
+        dataType: 'json',
         success: function(responseObject) {
           resolve(responseObject);
         },
