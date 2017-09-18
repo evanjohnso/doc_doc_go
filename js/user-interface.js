b24214b5e6a0e091b4e5172e5d63f440cb497674
ej <evanrjohnso@gmail.com>
2017-09-17 19:25:44 -0700

    implement try catch block to handle error appropriately

--- js/user-interface.js
+++ js/user-interface.js
@@ -20,20 +20,27 @@ getSpeciality()                       //load specialties with document
       .then(getDoctors)
       .then(displayDoctors)
       .catch( function (error) {
-        alert("Sorry, there seems to be an error", error);
+        alert("Sorry, there seems to be an error: " + error);
       });
   });
 
   function getLatLon(location, specialty) {
     return new Promise( function(resolve, reject) {
-    let geocoder = new google.maps.Geocoder();
-    geocoder.geocode( { address: location }, function(results) {
-      let lat = results[0].geometry.location.lat();
-      let lng = results[0].geometry.location.lng();
-      let coordinates = `${lat},${lng},15`; //last parameter is the search radius
-      resolve(coordinates, specialty);
-      reject("Google's geocoder failed, the servers are down. We're all fucked.");
-      });
+      try {
+        let geocoder = new google.maps.Geocoder();
+        geocoder.geocode( { address: location }, function(results) {
+          if (results.length == 0) {
+            throw new Error("Google's geocoder failed, the servers are down. We're all fucked.");
+            // reject("Google's geocoder failed, the servers are down. We're all fucked.");
+          }
+          let lat = results[0].geometry.location.lat();
+          let lng = results[0].geometry.location.lng();
+          let coordinates = `${lat},${lng},15`;         //last parameter is the search radius
+          resolve(coordinates, specialty);
+          });
+      } catch (e) {
+        console.log(e.message);
+      }
     });
   }
   function getDoctors(searchLocation, speciality) {
@@ -51,49 +58,57 @@ getSpeciality()                       //load specialties with document
         },
         dataType: 'json',
         success: function(responseObject) {
-          let scrubbedInfo = responseObject.data.map( function(doctor) {
-            return {
-              first: doctor.profile.first_name,
-              last: doctor.profile.last_name,
-              title: doctor.profile.title,
-              speciality: doctor.specialties[0].name,
-              picture: doctor.profile.image_url,
-              website: doctor.practices.website,
-              bio: doctor.profile.bio
-            };
+          console.log(responseObject);
+          try {
+            let scrubbedInfo = responseObject.data.map( function(doctor) {
+              return {
+                first: doctor.profile.first_name,
+                last: doctor.profile.last_name,
+                title: doctor.profile.title,
+                speciality: doctor.specialties[0].name,
+                picture: doctor.profile.image_url,
+                website: doctor.practices.website,
+                bio: doctor.profile.bio
+              };
+            });
+            resolve(scrubbedInfo);
+          } catch (e) {
+            console.log(e.message);
+          }
 
-          });
-
-          resolve(scrubbedInfo);
         },
         error: function(error) {
-          reject("API request for physician specialties failed, our bad");
+          reject("Request for specific doctors failed, our bad");
         }
       });
     });
   }
   function displayDoctors(jsonArray) {
-    console.log(jsonArray);
+    let $display = $('#outputs');
+    if (jsonArray.length == 0) {
+      return $display.html("<h1>Sorry, looks like we have no doctors. Please try your search again!<//h1>")
+                .css('text-align', 'center');
+    }
     jsonArray.forEach(function(doctor) {
+      //if string is too big, truncate to the closes end sentance
       let bioString = doctor.bio;
-      if (bioString.length > 700) { //if string is too big, find close period and end there
-        let endString = indexOf('.', 700);
+      if (bioString.length == 0) {
+        bioString = "No bio for this doctor available.";
+      } else if (bioString.length > 1000) {
+        let endString = bioString.indexOf('.', 1000);
         bioString = bioString.slice(0, endString);
       }
-        $('#outputs').append( `<div class='item'>
+        $display.append( `<div class='item'>
                                   <h3>${doctor.first} ${doctor.last}, ${doctor.title}</h3>
                                   <h4>${doctor.speciality}</h4>
                                   <img src="${doctor.picture}" alt='No picture to display'>
                                   <h5>${bioString}</h5>
                               </div>`
                             );
-
-      console.log(doctor);
     });
   }
   function displayDropdown(jsonArray) {
-      //split by dashes, join by spaces,
-      //uppercase first letter
+      //scrub data from (xx-xx-xx) to (Xx xx xx)
       let sorted = jsonArray.data.map( function(element) {
         let changing = element.uid.split('-').join(' ');
         return changing.toString().charAt(0).toUpperCase() + changing.toString().slice(1);
@@ -118,7 +133,7 @@ getSpeciality()                       //load specialties with document
           resolve(responseObject);
         },
         error: function(error) {
-          reject("API request for physician specialties failed, our bad");
+          reject("Request for medical specialties failed, our bad");
         }
       });
     });
