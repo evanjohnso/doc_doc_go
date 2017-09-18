360f4c8420b6d4fb74ef8ae51c9631754e1901c0
ej <evanrjohnso@gmail.com>
2017-09-18 12:48:59 -0700

    add google maps api

--- js/user-interface.js
+++ js/user-interface.js
@@ -38,7 +38,7 @@ logic.getSpeciality()                       //load specialties with document
       if (bioString.length == 0) {
         bioString = "No bio for this doctor available.";
       } else if (bioString.length > 700) {
-        let endString = bioString.indexOf('.', 700);
+        let endString = bioString.indexOf('.', 650);
         bioString = bioString.slice(0, endString);
       }
         $display.append( `<div class='item'>
@@ -84,17 +84,46 @@ logic.getSpeciality()                       //load specialties with document
     let activeOffices = doctorInfo.practices.filter (function(location) {
       return location.accepts_new_patients;
     });
-    console.log(activeOffices);
+    // console.log(activeOffices);
     let address = activeOffices[0].visit_address;
     address = `${address.street} ${address.city}, ${address.state} ${address.zip}`;
     let number = activeOffices[0].phones[0].number;
     number = `${number.slice(0,3)}-${number.slice(3,6)}-${number.slice(6)}`;
     let coords = `${activeOffices[0].lat}, ${activeOffices[0].lon}`;
+    let userLocation = sessionStorage.getItem('userLocation');
     $('#officeCoords').text(coords);
-    $('#userLocation').text(sessionStorage.getItem('userLocation'));
+    $('#userLocation').text(userLocation);
     $("#number").text(number);
     $('#address').text(address);
     $('#fakeModal').slideDown(1000);
     $('#fakeModal ~ ').fadeOut(1000);
+    userLocation = userLocation.slice(0, userLocation.length-3);
+    let split = userLocation.split(',');
+    let myObject = {
+      lat: parseFloat(split[0]),
+      lng: parseFloat(split[1])
+    };
+    let office = {
+      lat: parseFloat(`${activeOffices[0].lat}`),
+      lng: parseFloat(`${activeOffices[0].lon}`)
+    };
+    initializeMap(myObject, office);
     }
+    function initializeMap(ourLocation, officeLocation) {
+      let ourMap = new google.maps.Map(document.getElementById('bikeMap'), {
+      center: ourLocation,
+      zoom: 10
+    });
+    let anothermarker = new google.maps.Marker({
+       position: ourLocation,
+       map: ourMap,
+       title: 'Bike!'
+    });
+    let marker = new google.maps.Marker({
+       position: officeLocation,
+       map: ourMap,
+       title: 'Bike!'
+    });
+  }
+
 });
